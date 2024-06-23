c0e4c591d35d746ce711f80f0db715d3fa1c516f
Geshan Manandhar <geshan@simplywallst.com>
2024-06-23 20:30:41 +1000

    Adds sync example with finally

---
+++ sync-finally.js
@@ -0,0 +1,13 @@
+const jsonString = '{"key: "value"}';
+
+try {
+  const json = JSON.parse(jsonString);
+  console.info(json.key);
+} catch (err) {
+  console.error('Error parsing JSON string: ', err.message);
+  console.error('Error stack: ', err.stack);
+  console.error('Error name: ', err.name);
+  console.info(err instanceof SyntaxError); // true
+} finally {
+  console.info('This will always execute');
+}
